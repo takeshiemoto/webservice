d1e1d3b5e2b0acf2985526e8e0bfbd290c235b6a
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2020-12-13 00:16:59 +0900

    dev

--- server_test.go
+++ server_test.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandleGet(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/post/", handleRequest(&FakePost{}))
+
+	writer := httptest.NewRecorder()
+	request, _ := http.NewRequest("GET", "/post/1", nil)
+	mux.ServeHTTP(writer, request)
+
+	if writer.Code != 200{
+		t.Errorf("Response code is %v", writer.Code)
+	}
+
+	var post Post
+	json.Unmarshal(writer.Body.Bytes(), &post)
+	if post.ID != 1 {
+		t.Errorf("Cannot retrieve JSON post")
+	}
+}
